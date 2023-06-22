c683a51e397f10340a9b7690b74839a7ad35cf24
Maciej Kasperuk <maciejkas2000@gmail.com>
2023-06-22 20:56:16 +0200

    add put method

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,7 +1,7 @@
 <?php
-  
+
 namespace App\Controller;
-  
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * @Route("/api", name="api_")
  */
-  
+
 class DashboardController extends AbstractController
 {
     /**
@@ -29,30 +29,30 @@ class DashboardController extends AbstractController
         ]);
     }
     #[Route('/api', name: 'hotel_reservation_api_index', methods: ['GET'])]
-   
+
     public function index2(EntityManagerInterface $entityManager): Response
     {
         $hotelReservations = $entityManager->getRepository(Product::class)->findAll();
 
         return $this->json($hotelReservations);
     }
-    #[Route('/api', name:'post',  methods: ['POST'])]
-    public function createProduct(ManagerRegistry $doctrine,Request $request): Response
+    #[Route('/api', name: 'post',  methods: ['POST'])]
+    public function createProduct(ManagerRegistry $doctrine, Request $request): Response
     {
         $product = new Product();
         $data = json_decode($request->getContent(), true);
 
-        
+
         $product->setName($data['name']);
         $product->setDescription($data['description']);
         $product->setPrice($data['price']);
-        
+
         $entityManager = $doctrine->getManager();
         $entityManager->persist($product);
         $entityManager->flush($product);
 
         return $this->json([
-            'product'=>"Saved",
+            'product' => "Saved",
         ]);
     }
     #[Route('/api/{id}', name: 'patch', methods: ['PATCH'])]
@@ -75,5 +75,19 @@ class DashboardController extends AbstractController
             'message' => 'Product updated',
         ]);
     }
+    #[Route('/api/{id}', name: 'put', methods: ['PUT'])]
+    public function updateProduct(Request $request, Product $product, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
 
+        $product->setName($data['name'] ?? $product->getName());
+        $product->setDescription($data['description'] ?? $product->getDescription());
+        $product->setPrice($data['price'] ?? $product->getPrice());
+
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Product updated',
+        ]);
+    }
 }
